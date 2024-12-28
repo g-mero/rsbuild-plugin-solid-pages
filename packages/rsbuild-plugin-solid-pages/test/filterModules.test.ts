030680474f14aca6094b0f6f9805cee23d628686
g-mero <gmero@foxmail.com>
2024-12-28 14:17:53 +0800

    test: update test cases

--- packages/rsbuild-plugin-solid-pages/test/filterModules.test.ts
+++ packages/rsbuild-plugin-solid-pages/test/filterModules.test.ts
@@ -1,14 +1,29 @@
 import { expect, it } from 'vitest'
 import { filterExports } from '../src/module'
 
-it('should filter modules', () => {
-  const testCode = `
-    export const a = 1
-    export const b = 2`
+const testCode = `
+import 'style.css'
+const dead_code = 1
+export const a = 1
+const b = 2
+export { b }
+const c = 3
+export { c as d }
+export default c
+`
 
-  const result = filterExports(testCode, ['a'])
+it('filter css and default', () => {
+  const result = filterExports(testCode, ['default', 'css'])
 
   console.log(result)
 
-  expect(result).toBe('export const a = 1;\n')
+  expect(result).toBe('import"style.css";export default 3;')
+})
+
+it('filter named exports', () => {
+  const result = filterExports(testCode, ['a', 'd'])
+
+  console.log(result)
+
+  expect(result).toBe('export var a=1;var c=3;export{c as d};')
 })
